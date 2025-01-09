8638539c31e6353417303d935c5ec012fc7d36b0
Artem <135929723+Laterallamp@users.noreply.github.com>
2025-01-08 17:02:23 -0800

    Add files via upload

---
+++ src/NameSpaces.cpp
@@ -0,0 +1,22 @@
+ #include <iostream>
+// Namespace = provides a solution for preventing name conflicts in large projects. Each entity needs a unique name.
+// A namespace allows for identically named entities as long as the namespaces are different.
+
+ namespace first
+{
+  int x = 1;
+ }
+
+namespace second
+ {
+  int x = 2;
+ }
+
+ int main()
+ {
+  int x = 0;
+
+std::cout << second::x;
+
+  return 0;
+ }
